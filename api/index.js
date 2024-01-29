87acc24c474112954d22e132cd66e506c54eca12
Daniel Mendes de Albuquerque <daniel_developer@hotmail.com>
2024-01-28 22:08:45 -0300

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,23 @@
 import express from "express"
+import mongoose from "mongoose"
+import dotenv from "dotenv"
 
+dotenv.config()
+
+//connection to Database <>
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("MongoDB Connected!!")
+  })
+  .catch((err) => {
+    console.log(err)
+  })
+//</>
+
+//Express connection <>
 const app = express()
-
 app.listen(3000, () => {
   console.log("serving is running on port 3000")
 })
+//</>

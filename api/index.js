f0b591920dc8fa9e4471e7cb0330c8526dad53fa
Daniel Mendes de Albuquerque <daniel_developer@hotmail.com>
2024-02-26 16:14:46 -0300

    feat: create a post api route

--- api/index.js
+++ api/index.js
@@ -1,9 +1,10 @@
-import express from "express"
+import express, { application } from "express"
 import mongoose from "mongoose"
 import dotenv from "dotenv"
 import userRoutes from "./routes/user.route.js"
 import authRoutes from "./routes/auth.route.js"
 import cookieParser from "cookie-parser"
+import postRoutes from "./routes/post.route.js"
 
 dotenv.config()
 
@@ -31,8 +32,9 @@ app.listen(3000, () => {
 
 app.use("/api/user", userRoutes)
 app.use("/api/auth", authRoutes)
-
+app.use("/api/post", postRoutes)
 //middleware
+
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500
   const message = err.message || "Internal Server Error"

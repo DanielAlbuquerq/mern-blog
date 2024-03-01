deaf548fa5c04bfe15dcfb3dfac962034da72b3a
Daniel Mendes de Albuquerque <daniel_developer@hotmail.com>
2024-03-01 11:58:55 -0300

    posts inside dashboard with show more button

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -10,6 +10,7 @@ export default function DashPosts() {
 
   const { currentUser } = useSelector((state) => state.user)
   const [userPosts, setUserPosts] = useState([])
+  const [showMore, setShowMore] = useState(true)
 
   console.log("DashPost Activate")
 
@@ -21,6 +22,9 @@ export default function DashPosts() {
         console.log(data)
         if (res.ok) {
           setUserPosts(data.posts)
+          if (data.posts.length < 9) {
+            setShowMore(false)
+          }
         }
       } catch (error) {
         console.log(error.message)
@@ -33,6 +37,24 @@ export default function DashPosts() {
       fetchPosts()
     }
   }, [currentUser._id])
+
+  const handleShowMore = async () => {
+    const startIndex = userPosts.length
+    try {
+      const res = await fetch(
+        `/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`
+      )
+      const data = await res.json()
+      if (res.ok) {
+        setUserPosts((prev) => [...prev, ...data.posts])
+        if (data.posts.length < 9) {
+          setShowMore(false)
+        }
+      }
+    } catch (error) {
+      console.log(error.message)
+    }
+  }
 
   return (
     <div className="table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500">
@@ -91,6 +113,14 @@ export default function DashPosts() {
               </Table.Body>
             ))}
           </Table>
+          {showMore && (
+            <button
+              onClick={handleShowMore}
+              className="w-full hover:underline text-teal-500 self-center text-sm py-7"
+            >
+              Show more
+            </button>
+          )}
         </>
       ) : (
         <p> You have no post yet </p>

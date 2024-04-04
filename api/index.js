971b855fc7f4b4fbfa7f1e5d597f64eb5d4104b2
Daniel Mendes de Albuquerque <daniel_developer@hotmail.com>
2024-04-04 01:54:31 -0300

    Deployment Process

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ import authRoutes from "./routes/auth.route.js"
 import cookieParser from "cookie-parser"
 import postRoutes from "./routes/post.route.js"
 import commentRoutes from "./routes/comment.route.js"
+import path from 'path'
 
 dotenv.config()
 
@@ -20,6 +21,7 @@ mongoose
   })
 //</>
 
+  const __dirname = path.resolve()
 //Express connection <>
 const app = express()
 
@@ -36,7 +38,10 @@ app.use("/api/auth", authRoutes)
 app.use("/api/post", postRoutes)
 app.use("/api/comment", commentRoutes)
 
-
+app.use(express.static(path.join(__dirname, "/client/dist")))
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
+})
 //middleware
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500
